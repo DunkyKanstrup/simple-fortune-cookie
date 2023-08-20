890e4a29f8fd4784bd844cc7e1b93171e789ccf5
eivan20 <eivan20@student.sdu.dk>
2023-08-20 16:20:31 +0000

    Added curl-test testing job

--- ci/curl-test.sh
+++ ci/curl-test.sh
@@ -1,7 +1,7 @@
-
-RESPONECODE=$(curl -s -o /dev/null -w "%{http_code}" url:port)
-
-if [[ "$RESPONSECODE" == "200"]]
+#!/bin/bash
+RESPONSECODE=$(curl -s  -o /dev/null -w "%{http_code}" --connect-timeout 1 16.16.182.73:30023)
+echo "Http code: " $RESPONSECODE
+if [ $RESPONSECODE == '200' ];
 then
     echo "The site is up and running"
     exit 0
